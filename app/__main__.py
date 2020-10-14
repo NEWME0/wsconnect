a909a320175d4f2fa0d62ec0de62a2a3eecead00
artiom <ordersone@gmail.com>
2020-10-14 15:56:27 +0300

    Added typer

--- app/__main__.py
+++ app/__main__.py
@@ -1,4 +1,13 @@
 import uvicorn
 
 
-uvicorn.run('app:app', host='0.0.0.0', port=8000, ws='websockets')
+default = {
+    'app': 'app:app',
+    'host': '0.0.0.0',
+    'port': 8000,
+    'ws': 'websockets'
+}
+
+
+if __name__ == '__main__':
+    uvicorn.run(**default)
